e61f27c5e4f1cdb33360c7525288c548423383fb
karenelizabg <karene.gonzalez@iteso.mx>
2026-09-01 23:28:54 -0600

    feat: agregar CRUD de anotaciones (create/patch/delete)

---
+++ frontend/src/lib/api/annotations.ts
@@ -0,0 +1,40 @@
+import {
+  annotationSchema,
+  annotationsResponseSchema,
+  createAnnotationBodySchema,
+  patchAnnotationBodySchema,
+  type Annotation,
+  type CreateAnnotationBody,
+  type PatchAnnotationBody,
+} from "../../types/schemas";
+import { apiRequest, apiRequestVoid, jsonBody } from "./client";
+
+export function getAnnotations(imageId: number): Promise<Annotation[]> {
+  return apiRequest(`/images/${imageId}/annotations`, annotationsResponseSchema);
+}
+
+export function createAnnotation(
+  imageId: number,
+  body: CreateAnnotationBody
+): Promise<Annotation> {
+  const validBody = createAnnotationBodySchema.parse(body);
+  return apiRequest(`/images/${imageId}/annotations`, annotationSchema, {
+    method: "POST",
+    ...jsonBody(validBody),
+  });
+}
+
+export function updateAnnotation(
+  annotationId: number,
+  body: PatchAnnotationBody
+): Promise<Annotation> {
+  const validBody = patchAnnotationBodySchema.parse(body);
+  return apiRequest(`/annotations/${annotationId}`, annotationSchema, {
+    method: "PATCH",
+    ...jsonBody(validBody),
+  });
+}
+
+export function deleteAnnotation(annotationId: number): Promise<void> {
+  return apiRequestVoid(`/annotations/${annotationId}`, { method: "DELETE" });
+}
